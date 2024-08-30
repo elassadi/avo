815f543892425fad52d093e5cd6a57a93f65b841
Mohamed Elassadi <mohamed.elassadi@gmail.com>
2024-08-30 15:46:51 +0200

    Clean up model window and add keyevent listenener

--- app/components/avo/views/resource_edit_modal_component.rb
+++ app/components/avo/views/resource_edit_modal_component.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Avo::Views::ResourceEditModalComponent < Avo::Views::ResourceEditComponent
+  def initialize(resource: nil, model: nil, actions: [], view: :edit, parent_component: nil)
+    @parent_component = parent_component
+    super(resource: resource, model: model, actions: actions, view: view )
+  end
 end
--- app/javascript/js/controllers/modal_controller.js
+++ app/javascript/js/controllers/modal_controller.js
@@ -3,6 +3,16 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   static targets = ['modal']
 
+
+
+  connect() {
+    console.log('Modal controller connected')
+    this.setupKeyBindings()
+  }
+  disconnect() {
+    this.removeKeyBindings()
+  }
+
   close() {
     this.modalTarget.remove()
 
@@ -16,5 +26,33 @@ export default class extends Controller {
       vm.modalTarget.remove()
       document.dispatchEvent(new Event('actions-modal:close'))
     }, 500)
+  }
+
+
+  setupKeyBindings() {
+    this.form = this.modalTarget.querySelector('form')
+    if (this.form) {
+      this.keydownHandler = this.handleKeydown.bind(this)
+      document.addEventListener('keydown', this.keydownHandler)
+    }
+  }
+
+  removeKeyBindings() {
+    if (this.form && this.keydownHandler) {
+      document.removeEventListener('keydown', this.keydownHandler)
+    }
+  }
+
+  handleKeydown(event) {
+    console.log('handling keydown', event.key)
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      const submitButton = this.modalTarget.querySelector('button[type="submit"]')
+      if (submitButton) {
+        submitButton.click()
+      }
+    } else if (event.key === 'Escape') {
+      this.close()
+    }
   }
 }
